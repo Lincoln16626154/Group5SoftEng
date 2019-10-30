07cbc2a0dacdc321108aea10d28ddaa0b2fa83db
Lincoln16626154 <16626154@students.lincoln.ac.uk>
2019-10-30 12:56:50 +0000

    Add Textbox & layout example

--- UI.py
+++ UI.py
@@ -1,18 +1,31 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
 
 class GUI:
     def __init__(self, master):
         self.master = master
         master.title("The Application Title")
 
-        self.label = Label(master, text="This is a label")
-        self.label.pack()
+        self.exampleLabel = Label(master, text="This is a label")       
 
-        self.example_button = Button(master, text="This is button", command=self.buttonFunction)
-        self.example_button.pack()
+        self.exampleTextInput = Entry(master)
+        
+        self.exampleButton = Button(master, text="This is button", command=self.buttonFunction)
+        
+
+
+# LAYOUT
+
+        self.exampleLabel.grid(row=0, column=0, sticky=W)
+
+        self.exampleTextInput.grid(row=1, column=0, columnspan=3, sticky=W+E)
+
+        self.exampleButton.grid(row=2, column=0)
+
+
 
     def buttonFunction(self):
         print("button function called")
+
 
 root = Tk()
 my_gui = GUI(root)
